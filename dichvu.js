320e9ace24c09e0cedb29b405307718b3dc14782
vo minh duc <vominhduc760@gmail.com>
2023-01-07 21:59:08 +0700

    first commit

--- dichvu.js
+++ dichvu.js
@@ -15,6 +15,7 @@ const uri = `mongodb+srv://hv:vominhduc123@cluster0.zssl6ez.mongodb.net/test`;
 // Xây dựng Service
 const dichvu = http.createServer((req, res) => {
     let method = req.method;
+    console.log(method)
     let url = req.url;
     console.log(url)
     let kq = `Server NodeJS - Method:${method} - Url:${url}`;
@@ -106,7 +107,6 @@ const dichvu = http.createServer((req, res) => {
                 })
         }
         else if (url == "/Cuahang") {
-            console.log("hello")
             MongoClient.connect(uri)
                 .then(client => {
                     client.db(dbName).collection("Cua_hang").findOne()
@@ -157,14 +157,30 @@ const dichvu = http.createServer((req, res) => {
                     noi_dung: true
                 };
                 let user = JSON.parse(noi_dung_nhan);
+                MongoClient.connect(uri)
+                    .then(client => {
+                        client.db(dbName).collection("Admin").find().toArray()
+                            .then(result => {
+                                // arr = result;
+                                // arr = JSON.parse(strJSON);
+                                console.log(result)
+                                let vt = result.findIndex(item => item.Ten_Dang_nhap == user.Ten_Dang_nhap && item.Mat_khau == user.Mat_khau);
+                                if (vt == -1) {
+                                    kq.noi_dung = false
+                                }
+                                res.end(JSON.stringify(kq));
+                                client.close();
+                                // res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
+                                // res.end(kq)
+                            }).catch(err => {
+                                console.log(err)
+                            })
+                    }).catch(err => {
+                        console.log(err)
+                    })
                 // Read File Nguoi_dung
-                let strJSON = fs.readFileSync("./dulieu/Nguoi_dung.json");
-                let arr = JSON.parse(strJSON);
-                let vt = arr.findIndex(item => item.Ten_Dang_nhap == user.Ten_Dang_nhap && item.Mat_khau == user.Mat_khau);
-                if (vt == -1) {
-                    kq.noi_dung = false
-                }
-                res.end(JSON.stringify(kq));
+                // let strJSON = fs.readFileSync("./dulieu/Nguoi_dung.json");
+
             })
         }
         else if (url == "/sendMail") {
@@ -189,7 +205,6 @@ const dichvu = http.createServer((req, res) => {
             req.on("end", function () {
                 let thongTin = JSON.parse(noi_dung_nhan);
                 let Ket_qua = { "Noi_dung": true };
-                // let from = thongTin.email + "-" + thongTin.name;
                 let from = "";
                 let to = `ducvominh706@gmail.com`;
                 let subject = thongTin.email + "-" + thongTin.title;
@@ -203,17 +218,223 @@ const dichvu = http.createServer((req, res) => {
                     res.end(JSON.stringify(Ket_qua));
                 })
             })
+        } else if (url == "/ThemSanPham") {
+            req.on("end", () => {
+                let kq = {
+                    Noi_dung: true
+                };
+                let data = JSON.parse(noi_dung_nhan);
+                const key = data.group;
+                if (key === "Nhom_Dien_thoai") {
+                    var obj = {
+                        Ten: data.Ten,
+                        Ma_so: data.Ma_so,
+                        Don_gia_Ban: data.Don_gia_Ban,
+                        Don_gia_Nhap: data.Don_gia_Nhap,
+                        Nhom_Dien_thoai: {
+                            Ten: data[key].Ten,
+                            Ma_so: data[key].Ma_so
+                        },
+                        Danh_sach_Phieu_Dat: [],
+                        Danh_sach_Phieu_Ban: [],
+                        Danh_sach_Phieu_Nhap: [],
+                    }
+                } else if (key === "Nhom_Tivi") {
+                    var obj = {
+                        Ten: data.Ten,
+                        Ma_so: data.Ma_so,
+                        Don_gia_Ban: data.Don_gia_Ban,
+                        Don_gia_Nhap: data.Don_gia_Nhap,
+                        Nhom_Tivi: {
+                            Ten: data[key].Ten,
+                            Ma_so: data[key].Ma_so
+                        },
+                        Danh_sach_Phieu_Dat: [],
+                        Danh_sach_Phieu_Ban: [],
+                        Danh_sach_Phieu_Nhap: [],
+                    }
+                } else if (key === "Nhom_Mat_hang") {
+                    var obj = {
+                        Ten: data.Ten,
+                        Ma_so: data.Ma_so,
+                        Don_gia_Ban: data.Don_gia_Ban,
+                        Don_gia_Nhap: data.Don_gia_Nhap,
+                        Nhom_Mat_hang: {
+                            Ten: data[key].Ten,
+                            Ma_so: data[key].Ma_so
+                        },
+                        Danh_sach_Phieu_Dat: [],
+                        Danh_sach_Phieu_Ban: [],
+                        Danh_sach_Phieu_Nhap: [],
+                    }
+                }
+
+                MongoClient.connect(uri).then(client => {
+                    client.db(dbName).collection(data.collection).insertOne(obj).then(result => {
+                        // console.log(result)
+                        res.end(JSON.stringify(kq))
+                        client.close();
+                    }).catch(err => {
+                        console.log(err);
+                        kq.Noi_dung = false;
+                        res.end(JSON.stringify(kq));
+                    })
+                }).catch(err => {
+                    console.log(err);
+                })
+            })
+        }
+        else if (url == "/ImagesSanpham") {
+            req.on('end', function () {
+                let img = JSON.parse(noi_dung_nhan);
+                let kq = { Noi_dung: true };
+                let kqImg = saveMedia(img.name, img.src);
+                if (kqImg == "OK") {
+                    // res.writeHead(200, { "Content-Type": "text/json; charset=utf-8" });
+                    res.end(JSON.stringify(kq));
+                } else {
+                    kq.Noi_dung = false
+                    // res.writeHead(200, { "Content-Type": "text/json; charset=utf-8" });
+                    res.end(JSON.stringify(kq));
+                }
+                // imgCloud.UPLOAD_CLOUDINARY(img.name,img.src).then(result=>{
+                //     console.log(result);
+                //     Dap_ung.end(JSON.stringify(Ket_qua));
+
+                // }).catch(err=>{
+                //     Ket_qua.Noi_dung=false
+                //     Dap_ung.end(JSON.stringify(Ket_qua))
+                // })
+            })
+        }
+        else if (url == "/updateProduct") {
+            req.on("end", () => {
+                let kq = { Noi_dung: true };
+                let data = JSON.parse(noi_dung_nhan);
+                if (data.group === "Nhom_Tivi") {
+                    var mobileUpdate = {
+                        condition: {
+                            _id: mongo_db.ObjectId(data._id),
+                        },
+                        update: {
+                            $set: {
+                                Ten: data.Ten,
+                                Ma_so: data.Ma_so_temp,
+                                Don_gia_Ban: data.Don_gia_Ban,
+                                Don_gia_Nhap: data.Don_gia_Nhap,
+                                Nhom_Tivi: {
+                                    Ten: data[data.group].Ten,
+                                    Ma_so: data[data.group].Ma_so
+                                }
+                            }
+                        }
+                    }
+                } else if (data.group === "Nhom_Dien_thoai") {
+                    var mobileUpdate = {
+                        condition: {
+                            _id: mongo_db.ObjectId(data._id),
+                        },
+                        update: {
+                            $set: {
+                                Ten: data.Ten,
+                                Ma_so: data.Ma_so_temp,
+                                Don_gia_Ban: data.Don_gia_Ban,
+                                Don_gia_Nhap: data.Don_gia_Nhap,
+                                Nhom_Dien_thoai: {
+                                    Ten: data[data.group].Ten,
+                                    Ma_so: data[data.group].Ma_so
+                                }
+                            }
+                        }
+                    }
+                } else if (data.group === "Nhom_Mat_hang") {
+                    var mobileUpdate = {
+                        condition: {
+                            _id: mongo_db.ObjectId(data._id),
+                        },
+                        update: {
+                            $set: {
+                                Ten: data.Ten,
+                                Ma_so: data.Ma_so_temp,
+                                Don_gia_Ban: data.Don_gia_Ban,
+                                Don_gia_Nhap: data.Don_gia_Nhap,
+                                Nhom_Mat_hang: {
+                                    Ten: data[data.group].Ten,
+                                    Ma_so: data[data.group].Ma_so
+                                }
+                            }
+                        }
+                    }
+                }
+                console.log(mobileUpdate)
+                MongoClient.connect(uri).then(client => {
+                    client.db(dbName).collection(data.collection).updateOne(mobileUpdate.condition, mobileUpdate.update).then(result => {
+                        // console.log(result)
+                        res.end(JSON.stringify(kq))
+                        client.close();
+                    }).catch(err => {
+                        // console.log(err);
+                        kq.Noi_dung = false;
+                        res.end(JSON.stringify(kq));
+                    })
+                }).catch(err => {
+                    console.log(err);
+                })
+            })
+        } else if (url == "/removeProduct") {
+            req.on("end", () => {
+                let kq = { Noi_dung: true };
+                let data = JSON.parse(noi_dung_nhan);
+                MongoClient.connect(uri).then(client => {
+                    client.db(dbName).collection(data.collection).deleteOne({ _id: mongo_db.ObjectId(data._id) }).then(result => {
+                        // console.log(result)
+                        res.end(JSON.stringify(kq))
+                        client.close();
+                    }).catch(err => {
+                        // console.log(err);
+                        kq.Noi_dung = false;
+                        res.end(JSON.stringify(kq));
+                    })
+                }).catch(err => {
+                    console.log(err);
+                })
+            })
         }
         else {
             kq = `Method: ${method} - Chưa xét URL ${url}`
             res.end(kq)
         }
-
     }
     else {
         res.end(kq);
     }
 })
+
+function decodeBase64Image(dataString) {
+    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+    if (matches.length !== 3) {
+        return new Error('Error ...');
+    }
+
+    response.type = matches[1];
+    response.data = new Buffer(matches[2], 'base64');
+
+    return response;
+}
+
+function saveMedia(Ten, Chuoi_nhi_phan) {
+    var Kq = "OK"
+    try {
+        var Nhi_phan = decodeBase64Image(Chuoi_nhi_phan);
+        var Duong_dan = "images//" + Ten + ".png"
+        fs.writeFileSync(Duong_dan, Nhi_phan.data);
+    } catch (Loi) {
+        Kq = Loi.toString();
+    }
+    return Kq
+}
 // Dịch vụ lắng nghe tại cổng nào
 dichvu.listen(port, () => {
     console.log(`Server Run.. http://localhost:${port}`)
